1cd2c5edba42dcbf5717667aa2400eb0d9ec74ee
Yao Li <hnkfliyao@gmail.com>
2015-05-07 21:18:45 +0200

    Correct the name.

--- project/build.scala
+++ project/build.scala
@@ -4,11 +4,11 @@ import Tests._
 
 object migrationBuild extends Build {
   lazy val coreProject = Project("migrations-core", file("core"))
-  lazy val sqlMigrationProject = Project(
+  lazy val slickMigrationProject = Project(
     "slick-migrations", file("migrations/slick")) dependsOn(coreProject)
   lazy val plainMigrationProject = Project(
     "plain-migrations", file("migrations/plain")) dependsOn(coreProject)
   lazy val exampleProject = Project(
     "example", file("example")) dependsOn(
-    coreProject, sqlMigrationProject, plainMigrationProject)
+    coreProject, slickMigrationProject, plainMigrationProject)
 }
