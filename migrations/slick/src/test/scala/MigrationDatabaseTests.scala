5f6781bdac9294bc0140571ab19cacfedbb9c854
Yao Li <hnkfliyao@gmail.com>
2015-07-16 16:11:36 +0800

    Better naming in tests.

--- migrations/slick/src/test/scala/MigrationDatabaseTests.scala
+++ migrations/slick/src/test/scala/MigrationDatabaseTests.scala
@@ -7,18 +7,24 @@ import collection.mutable.ListBuffer
 import scala.migrations.slick.tools.git.H2MigrationDatabase
 
 case class TestDir(path: String) {
-  val upDir = new File(path)
-  if (!upDir.exists) upDir.mkdir()
   val testPath = path + "/test"
-  lazy val file = {
-    val testDir = new File(testPath)
-    if (!testDir.exists) testDir.mkdir()
-    testDir
+
+  lazy val dir = {
+    val file = new File(path)
+    if (!file.exists) file.mkdir()
+    file
+  }
+
+  lazy val testDir = {
+    dir
+    val file = new File(testPath)
+    if (!file.exists) file.mkdir()
+    file
   }
 
   def setup() {
     val examples = new File(System.getProperty("user.dir") + "/example")
-    FileUtils.copyDirectory(examples, file)
+    FileUtils.copyDirectory(examples, testDir)
   }
 
   def clean() {
@@ -42,7 +48,7 @@ class MigrationDatabaseTest extends FlatSpec
   val objDir = dir.testPath + "/.db"
 
   before {
-    assert(dir.file.isDirectory === true)
+    assert(dir.testDir.isDirectory === true)
     dir.setup()
     FileUtils.deleteDirectory(new File(objDir))
   }
@@ -51,20 +57,20 @@ class MigrationDatabaseTest extends FlatSpec
     dir.clean()
   }
 
-  def inDir(commands: Seq[String]) = {
-    Process(commands, dir.file)
+  def inTestDir(commands: Seq[String]) = {
+    Process(commands, dir.testDir)
   }
 
-  def inUpDir(commands: Seq[String]) = {
-    Process(commands, dir.upDir)
+  def inDir(commands: Seq[String]) = {
+    Process(commands, dir.dir)
   }
 
   "copy" should "commit db into .db" in {
-    assert(inUpDir(Seq("git", "init")).! === 0)
-    assert(inDir(Seq("sbt", "git-tools/run install")).! === 0)
-    assert(inDir(Seq("sbt", "git-tools/run rebuild")).! === 0)
-    assert(inUpDir(Seq("git", "add", ".")).! === 0)
-    assert(inUpDir(Seq("git", "commit", "-m", "test")).! === 0)
+    assert(inDir(Seq("git", "init")).! === 0)
+    assert(inTestDir(Seq("sbt", "git-tools/run install")).! === 0)
+    assert(inTestDir(Seq("sbt", "git-tools/run rebuild")).! === 0)
+    assert(inDir(Seq("git", "add", ".")).! === 0)
+    assert(inDir(Seq("git", "commit", "-m", "test")).! === 0)
     val dbFile = new File(objDir + "/master/db")
     assert(dbFile.exists === true)
     assert(dbFile.isFile === true)
