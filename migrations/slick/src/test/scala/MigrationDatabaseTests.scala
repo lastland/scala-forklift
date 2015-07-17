2827174135deface37b1105b7a5806ae243d1368
Yao Li <hnkfliyao@gmail.com>
2015-07-17 14:35:47 +0800

    Tests for "merge".

--- migrations/slick/src/test/scala/MigrationDatabaseTests.scala
+++ migrations/slick/src/test/scala/MigrationDatabaseTests.scala
@@ -141,4 +141,41 @@ class MigrationDatabaseTest extends FlatSpec
     assert(FileUtils.contentEquals(db,
       new File(objDir + "/test/db")) === true)
   }
+
+  "merge" should "use the db of first parent" in {
+    val m3Name = dir.testPath + "/migrations/src_migrations/main/scala/3.scala"
+    val m3DisabledName = m3Name + ".disabled"
+    Given("an example project with two branches with different db")
+    assert(runInDir(Seq("git", "init")) === 0)
+    FileUtils.moveFile(new File(m3Name), new File(m3DisabledName))
+    assert(runInTestDir(Seq("sbt", "git-tools/run install")) === 0)
+    assert(runInTestDir(Seq("sbt", "git-tools/run rebuild")) === 0)
+    FileUtils.copyFileToDirectory(new File(".gitignore"), dir.dir)
+    assert(runInDir(Seq("git", "add", ".")) === 0)
+    assert(runInDir(Seq("git", "commit", "-m", "initial")) === 0)
+    assert(runInDir(Seq("git", "checkout", "-b", "test")) === 0)
+    FileUtils.moveFile(new File(m3DisabledName), new File(m3Name))
+    assert(runInTestDir(Seq("sbt", "git-tools/run rebuild")) === 0)
+    assert(runInDir(Seq("git", "add", ".")) === 0)
+    assert(runInDir(Seq("git", "commit", "-m", "test")) === 0)
+    assert(runInDir(Seq("git", "checkout", "master")) === 0)
+    val tmpFile = new File(dir.testDir + "/test_file")
+    tmpFile.createNewFile()
+    assert(runInDir(Seq("git", "add", tmpFile.getAbsolutePath)) === 0)
+    assert(runInDir(Seq("git", "commit", "-m", "master")) === 0)
+
+    When("two branches are merged")
+    assert(runInDir(Seq("git", "merge", "test")) === 0)
+
+    Then("the db should be in test directory")
+    val db = new File(dir.testPath + "/test.tb.h2.db")
+    assert(db.exists === true)
+    assert(db.isFile === true)
+
+    And("the db is identical to that from master but different from that from test")
+    assert(FileUtils.contentEquals(db,
+      new File(objDir + "/master/db")) === true)
+    assert(FileUtils.contentEquals(db,
+      new File(objDir + "/test/db")) === false)
+  }
 }
