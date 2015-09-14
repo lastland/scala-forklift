0d4976ebd33714359d9b5a6f21cfbc66249a398e
Yao Li <hnkfliyao@gmail.com>
2015-09-14 15:36:10 +0800

    Use snapshots.

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object migrationBuild extends Build {
     organization := "com.liyaos",
     licenses := Seq("BSD-2-Clause" -> url("https://github.com/lastland/scala-forklift/blob/master/LICENSE")),
     homepage := Some(url("https://github.com/lastland/scala-forklift")),
-    version := "0.2.0-ALPHA",
+    version := "0.2.0-SNAPSHOT",
     scalaVersion := "2.11.6",
     scalacOptions += "-deprecation",
     scalacOptions += "-feature",
