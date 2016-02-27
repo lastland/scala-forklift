b16a89b40bc85be86541bbc5a4ddeb975f283d1a
Yao Li <hnkfliyao@gmail.com>
2016-02-28 02:48:37 +0800

    Bump versions.

--- example/project/build.scala
+++ example/project/build.scala
@@ -25,7 +25,7 @@ object AppBuild extends Build {
   )
 
   lazy val forkliftDependencies = List(
-    "com.liyaos" %% "scala-forklift-slick" % "0.2.1"
+    "com.liyaos" %% "scala-forklift-slick" % "0.2.2-SNAPSHOT"
   )
 
   lazy val appDependencies = dbDependencies ++ loggingDependencies
@@ -61,7 +61,7 @@ object AppBuild extends Build {
   lazy val tools = Project("git-tools",
     file("tools/git")).settings(commonSettings:_*).settings {
     libraryDependencies ++= forkliftDependencies ++ List(
-      "com.liyaos" %% "scala-forklift-git-tools" % "0.2.1",
+      "com.liyaos" %% "scala-forklift-git-tools" % "0.2.2-SNAPSHOT",
       "com.typesafe" % "config" % "1.3.0",
       "org.eclipse.jgit" % "org.eclipse.jgit" % "4.0.1.201506240215-r"
     )
