d576ee636c55a737cb2196a2c975f63b42ab64f5
Guillaume George <lysandergc@gmail.com>
2018-11-01 10:44:01 +0900

    Use MyMigrationManager in MyMigrations
    `MyMigrationManager` is defined in the `migration_manager` project but not used.

--- example/migrations/src/main/scala/Migrations.scala
+++ example/migrations/src/main/scala/Migrations.scala
@@ -1,9 +1,10 @@
 import com.liyaos.forklift.slick._
+import example.migration.manager.MyMigrationManager
 
 object MyMigrations extends App
     with SlickMigrationCommandLineTool
     with SlickMigrationCommands
-    with SlickMigrationManager
+    with MyMigrationManager
     with Codegen {
   MigrationSummary
   execCommands(args.toList)
